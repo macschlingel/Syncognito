3ceb7134a1852f805139faf24b7c136f7b6ab53f
Bastian Wölfle <info@schlingel.de>
2026-03-04 12:21:28 +0100

    Initial Commit

---
+++ Syncognito/SyncognitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncognitoApp.swift
+//  Syncognito
+//
+//  Created by Bastian Wölfle on 04.03.26.
+//
+
+import SwiftUI
+
+@main
+struct SyncognitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SyncognitoTests/SyncognitoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SyncognitoTests.swift
+//  SyncognitoTests
+//
+//  Created by Bastian Wölfle on 04.03.26.
+//
+
+import Testing
+@testable import Syncognito
+
+struct SyncognitoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
